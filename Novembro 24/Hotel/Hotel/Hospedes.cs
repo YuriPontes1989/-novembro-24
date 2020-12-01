db66615c51201de42821c7b5773455ab2ec35fa5
BloodyValentine <69511939+YuriPontes1989@users.noreply.github.com>
2020-12-01 17:39:09 -0300

    Add files via upload

---
+++ Novembro 24/Hotel/Hotel/Hospedes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+
+{
+    class Hospedes
+    {
+
+        string nome;
+        string email;
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+
+    }
+}
+
